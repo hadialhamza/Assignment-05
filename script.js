ae80c5f9c94c05706fc26c40e91dd6255e4a6ffe
hadialhamza <hamzaglory@gmail.com>
2025-08-28 02:37:48 +0600

    Heart icon functionality added

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log("script file loaded");
+// Function for Get ID
+function getId(id) {
+  return document.getElementById(id);
+}
 
+// Function for Get Class
+function getClass(className) {
+  return document.getElementsByClassName(className);
+}
+
+// Heart Icon Functionality
+const heartIcons = getClass("heart-btn");
+for (icon of heartIcons) {
+  icon.addEventListener("click", function () {
+    const heartCount = parseInt(getId("heart-counter").innerText);
+    const newHeartCount = heartCount + 1;
+    getId("heart-counter").innerText = newHeartCount;
+  });
+}
