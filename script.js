830aa863f2cfbd107010ca81ef4fe64775a8de4e
hadialhamza <hamzaglory@gmail.com>
2025-08-28 05:32:50 +0600

    clear history button functionality added

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ for (const icon of heartIcons) {
 const callButtons = getClass("call-button");
 for (const button of callButtons) {
   button.addEventListener("click", function () {
+    // Coin Validation
     const balance = parseInt(getId("balance").innerText);
     if (balance < 20) {
       alert(
@@ -29,10 +30,38 @@ for (const button of callButtons) {
       );
       return;
     }
+
+    // Calling Functionality
     const serviceName = button.parentNode.parentNode.children[2].innerText;
     const serviceNumber = button.parentNode.parentNode.children[3].innerText;
     alert(`📞 Calling ${serviceName} ${serviceNumber} ...`);
+
     const newBalance = balance - 20;
     getId("balance").innerText = newBalance;
+
+    // Calling History
+    const currentTime = new Date().toLocaleTimeString();
+    const callHistoryContainer = getId("call-history-container");
+    const newElement = document.createElement("div");
+    newElement.innerHTML = `
+    <div class="bg-[#fafafa] p-4 my-3 flex justify-between items-center rounded-lg">
+         <div class="w-[65%]">
+           <h2 class="font-inter font-semibold text-lg">${serviceName}</h2>
+           <p class="font-hind-madurai text-lg text-[#5c5c5c]">${serviceNumber}</p>
+         </div>
+         <div>
+           <span class="font-hind-madurai text-lg text-[#111111]">${currentTime}</span>
+         </div>
+       </div>
+       `;
+
+    callHistoryContainer.appendChild(newElement);
   });
 }
+
+// History Clear Button Functionality
+const historyClearBtn = getId("history-clear-btn");
+historyClearBtn.addEventListener("click", function () {
+  const callHistoryContainer = getId("call-history-container");
+  callHistoryContainer.innerHTML = "";
+});
