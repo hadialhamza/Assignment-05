89c88b715add7b8bb92a0f87a5695778842c8e6f
hadialhamza <hamzaglory@gmail.com>
2025-08-28 04:46:24 +0600

    call button alert, reduce coin and low coin alert added

--- script.js
+++ script.js
@@ -10,10 +10,29 @@ function getClass(className) {
 
 // Heart Icon Functionality
 const heartIcons = getClass("heart-btn");
-for (icon of heartIcons) {
+for (const icon of heartIcons) {
   icon.addEventListener("click", function () {
     const heartCount = parseInt(getId("heart-counter").innerText);
     const newHeartCount = heartCount + 1;
     getId("heart-counter").innerText = newHeartCount;
+  });
+}
+
+// Call Button Functionality
+const callButtons = getClass("call-button");
+for (const button of callButtons) {
+  button.addEventListener("click", function () {
+    const balance = parseInt(getId("balance").innerText);
+    if (balance < 20) {
+      alert(
+        "❌ You do not have enough coins to make a call! You need minimum 20 coins to make a call."
+      );
+      return;
+    }
+    const serviceName = button.parentNode.parentNode.children[2].innerText;
+    const serviceNumber = button.parentNode.parentNode.children[3].innerText;
+    alert(`📞 Calling ${serviceName} ${serviceNumber} ...`);
+    const newBalance = balance - 20;
+    getId("balance").innerText = newBalance;
   });
 }
